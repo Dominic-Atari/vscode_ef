6d8086bbd97df0d1ceba721189ece59cfd3558a0
Dominic-Atari <kikosimikehop2@gmail.com>
2025-08-03 11:22:12 -0500

    main commit

--- obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab7cea411edaab38480661d653aaa8711a6c14d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0276e731571e5a114adf8a20f55b6835c853e8d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
